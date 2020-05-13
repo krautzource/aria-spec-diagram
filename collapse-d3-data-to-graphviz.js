d1d1235549dec38cb4ea2359fddd1f921903bcd4
Peter Krautzberger <p.krautzberger@gmail.com>
2020-05-13 21:02:38 +0200

    feat: add graphviz example
    * collapse-d3-data-to_graphviz: generates graphviz-like output
    * graphviz.dot: the resulting output
    * mermaid-flowchart.svg: mermaid output

---
+++ collapse-d3-data-to-graphviz.js
@@ -0,0 +1,15 @@
+const data = require('./d3dendogram-data.json');
+
+const collapse = (resultArray, parent, child) => {
+  if (parent) resultArray.push(`${parent.name} --> ${child.name};`);
+  if (child.children.length < 0) return;
+  child.children.forEach(collapse.bind(null, resultArray, child));
+};
+
+let results = [];
+
+collapse(results, null, data);
+
+console.log(results.join('\n'));
+// pipe output to, e.g.,  https://mermaid-js.github.io/mermaid-live-editor/ (as flow diagram)
+// or (with modification '-->' to '->' ) to https://dagrejs.github.io/project/dagre-d3/latest/demo/interactive-demo.html
